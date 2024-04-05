f48b440386359f391d0be285da9707fb2a3b7a6b
555Jeka555 <2003antyshev@mail.ru>
2024-04-05 19:05:41 +0300

    Фикс запроса

--- lab6.sql
+++ lab6.sql
@@ -1,4 +1,21 @@
 # 1 Добавить внешние ключи
+ALTER TABLE booking
+    ADD CONSTRAINT `booking_client_id_client_fk`
+        FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`);
+
+ALTER TABLE room
+    ADD CONSTRAINT `room_hotel_id_hotel_fk`
+        FOREIGN KEY (`id_hotel`) REFERENCES `hotel` (`id_hotel`);
+ALTER TABLE room
+    ADD CONSTRAINT `room_room_category_id_room_category_fk`
+        FOREIGN KEY (`id_room_category`) REFERENCES `room_category` (`id_room_category`);
+
+ALTER TABLE room_in_booking
+    ADD CONSTRAINT `room_in_booking_room_id_room_fk`
+        FOREIGN KEY (`id_room`) REFERENCES `room` (`id_room`);
+ALTER TABLE room_in_booking
+    ADD CONSTRAINT `room_in_booking_booking_id_booking_fk`
+        FOREIGN KEY (`id_booking`) REFERENCES `booking` (`id_booking`);
 
 
 # 2 Выдать информацию о клиентах гостиницы “Космос”, проживающих в номерах
@@ -42,6 +59,12 @@ FROM client c
          JOIN hotel h USING (id_hotel)
 WHERE h.name = 'Космос'
   AND MONTH(rib.checkout_date) = 4
+  AND rib.checkout_date = (SELECT MAX(rib2.checkout_date)
+                           FROM room_in_booking rib2
+                                    JOIN booking b2 USING (id_booking)
+                                    JOIN client c2 USING (id_client)
+                           WHERE c.id_client = c2.id_client
+                             AND MONTH(rib2.checkout_date) = 4)
 ORDER BY rib.checkout_date DESC;
 
 # 6 Продлить на 2 дня дату проживания в гостинице “Космос” всем клиентам
@@ -83,14 +106,14 @@ WHERE rib1.checkin_date < rib2.checkout_date
 START TRANSACTION;
 
 INSERT INTO client (name, phone)
-VALUES ('Боба', '123456789');
+VALUES ('Боба6', '123456789');
 
 SET @client_id = LAST_INSERT_ID();
 
 INSERT INTO booking (id_client, booking_date)
 VALUES (@client_id, '2019-01-10');
 
-SET @booking_id = LAST_INSERT_ID();
+SET @booking_id = 999999;
 
 INSERT INTO room_in_booking (id_booking, id_room, checkin_date, checkout_date)
 VALUES (@booking_id, @client_id, '2024-04-01', '2024-04-05');
@@ -99,4 +122,30 @@ COMMIT;
 ROLLBACK;
 
 #9 Добавить необходимые индексы для всех таблиц
+CREATE INDEX hotel_name_idx ON hotel (name(50));
 
+CREATE INDEX room_category_name_idx ON room_category (name(50));
+
+CREATE INDEX booking_id_client_idx ON booking (id_client);
+
+CREATE INDEX room_id_hotel_idx ON room (id_hotel);
+CREATE INDEX room_id_room_category_idx ON room (id_room_category);
+
+CREATE INDEX room_in_booking_id_room_idx ON room_in_booking (id_room);
+CREATE INDEX room_in_booking_id_booking_idx ON room_in_booking (id_booking);
+CREATE INDEX room_in_booking_checkin_date_checkout_date_idx ON room_in_booking (checkin_date, checkout_date);
+CREATE INDEX room_in_booking_checkout_date_checkin_date_idx ON room_in_booking (checkout_date, checkin_date);
+
+DROP INDEX hotel_name_idx ON hotel;
+
+DROP INDEX room_category_name_idx ON room_category;
+
+DROP INDEX booking_id_client_idx ON booking;
+
+DROP INDEX room_id_hotel_idx ON room;
+DROP INDEX room_id_room_category_idx ON room;
+
+DROP INDEX room_in_booking_id_room_idx ON room_in_booking;
+DROP INDEX room_in_booking_id_booking_idx ON room_in_booking;
+DROP INDEX room_in_booking_checkin_date_checkout_date_idx ON room_in_booking;
+DROP INDEX room_in_booking_checkout_date_checkin_date_idx ON room_in_booking;
